a980f1641747a2afdff934febbd2a4a1d446620b
Joao Felipe Silveira <joaofe747@gmail.com>
2021-11-12 17:06:14 -0500

    flags showing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
 import { listLogEntries } from "./FrontApi";
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -15,7 +16,7 @@ const App = () => {
   useEffect(() => {
     (async () => {
       const logEntries = await listLogEntries();
-      console.log(logEntries);
+      setLogEntries(logEntries);
     })();
   }, []);
 
@@ -27,9 +28,43 @@ const App = () => {
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
         mapStyle="mapbox://styles/accessjoao/ckvwr2unz0sgb14oa0tsnxngk"
-      />
+      >
+        {logEntries.map((entry) => (
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+            offsetLeft={-5}
+            offsetTop={-20}
+          >
+            <div>
+              <svg
+                className="marker"
+                style={{
+                  width: "24px",
+                  height: "24px",
+                }}
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path>
+                <line x1="4" y1="22" x2="4" y2="15"></line>
+              </svg>
+            </div>
+          </Marker>
+        ))}
+      </ReactMapGL>
     </div>
   );
 };
 
 export default App;
+
+/*
+
+<path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path>
+
+*/
